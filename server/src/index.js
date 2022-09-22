67b083e911b0977a72ecc13ac79b5df61fc22628
”Bburn1” <bogdanburn123@gmail.com>
2022-09-22 13:34:54 +0300

    j

--- server/src/index.js
+++ server/src/index.js
@@ -10,76 +10,76 @@ const { Op } = require('sequelize');
     console.error('unable to authenticate:', error.message)
   }
 
-  //1. На получение всех данных из таблиц
+  // //1. На получение всех данных из таблиц
 
-  try {
-    const brandsAll = await db.Brand.findAll({ raw: true })
-    console.log(JSON.stringify(brandsAll, null, 2))
-  } catch (error) {
-    console.error('Bad request:', error.message)
-  }
+  // try {
+  //   const brandsAll = await db.Brand.findAll({ raw: true })
+  //   console.log(JSON.stringify(brandsAll, null, 2))
+  // } catch (error) {
+  //   console.error('Bad request:', error.message)
+  // }
 
-  //////////////////////
+  // //////////////////////
 
-  //2. Получение данных для id больше
+  // //2. Получение данных для id больше
 
-  try {
-    const items = await db.Item.findAll({
-      where: {
-        id: { [Op.gt]: 10 },
-      },
-    })
-      console.log(JSON.stringify(items, null, 2))
-  } catch (error) {
-    console.error('Bad request:', error.message)
-  }
+  // try {
+  //   const items = await db.Item.findAll({
+  //     where: {
+  //       id: { [Op.gt]: 10 },
+  //     },
+  //   })
+  //     console.log(JSON.stringify(items, null, 2))
+  // } catch (error) {
+  //   console.error('Bad request:', error.message)
+  // }
 
-  //3. Получение набора данных для нескольких значений одного атрибута.
+  // //3. Получение набора данных для нескольких значений одного атрибута.
 
-  try {
-    const models = await db.Model.findAll({
-      where: {
-        brand_id: [2, 0],
-      },
-    })
-    console.log(JSON.stringify(models, null, 2))
+  // try {
+  //   const models = await db.Model.findAll({
+  //     where: {
+  //       brand_id: [2, 0],
+  //     },
+  //   })
+  //   console.log(JSON.stringify(models, null, 2))
 
-  } catch (error) {
-  console.error('Bad request:', error.message)
+  // } catch (error) {
+  // console.error('Bad request:', error.message)
 
-  }
+  // }
 
-  //4.Прописать запрос на удаление данных по массиву значений атрибута.
+  // //4.Прописать запрос на удаление данных по массиву значений атрибута.
 
-  try {
-    const deleteItems = await db.Item.destroy({
-      where: {
-        brand_id: [2, 0],
-      },
-    })
+  // try {
+  //   const deleteItems = await db.Item.destroy({
+  //     where: {
+  //       brand_id: [2, 0],
+  //     },
+  //   })
   
 
-  } catch (error) {
-    console.error('Bad request:', error.message)
-  }
+  // } catch (error) {
+  //   console.error('Bad request:', error.message)
+  // }
 
-  //5.Прописать запрос на изменение данных.
+  // //5.Прописать запрос на изменение данных.
 
-  const updatedStore = {
-    title: 'UpdatedStore',
-  }
+  // const updatedStore = {
+  //   title: 'UpdatedStore',
+  // }
 
-  try {
-    const updateStore = await db.Store.update(updatedStore, {
-      where: {
-        title: 'Store #0',
-      },
-    })
+  // try {
+  //   const updateStore = await db.Store.update(updatedStore, {
+  //     where: {
+  //       title: 'Store #0',
+  //     },
+  //   })
      
 
-  } catch (error) {
-    console.error('Bad request:', error.message)
-  }
+  // } catch (error) {
+  //   console.error('Bad request:', error.message)
+  // }
 
   //////////////
 })()
